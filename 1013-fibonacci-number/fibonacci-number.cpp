4087d931808dc5862d9f97832df57f9a6375e27d
ajinkyawasnik04-lab <ajinkyawasnik04@gmail.com>
2026-06-30 23:58:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (37.62%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n==0)
-        return 0;
-        if (n==1)
-        return 1;
-
-        return fib(n-1)+fib(n-2);
+        if (n <= 1) return n;
         
+        int a = 0; // F(0)
+        int b = 1; // F(1)
+        int c = 0; // Will hold F(n)
+        
+        for (int i = 2; i <= n; i++) {
+            c = a + b; // Current is sum of past two
+            a = b;     // Move 'a' forward
+            b = c;     // Move 'b' forward
+        }
+        
+        return b;
     }
 };
