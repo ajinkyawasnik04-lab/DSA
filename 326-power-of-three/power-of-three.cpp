1654f0d7779c9fb90f186005295e0b13fff7d5a7
ajinkyawasnik04-lab <ajinkyawasnik04@gmail.com>
2026-07-01 00:11:16 +0530

    Time: 4 ms (45.10%) | Memory: 8.8 MB (97.77%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        
-        if (n <= 0) return false;
-        
-        if (n == 1) return true;
-        
-     
-        if (n % 3 == 0) {
-            return isPowerOfThree(n / 3);
-        }
-        
-        
-        return false;
+       
+        return n > 0 && 1162261467 % n == 0;
     }
 };
